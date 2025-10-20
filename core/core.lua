23c6d9f6daee5cf0cec300a476a00cec36b4d736
Ungweliant <>
2025-10-21 01:46:35 +0200

    first steps

--- core/core.lua
+++ core/core.lua
@@ -1,18 +1,4 @@
-UngweUI = {}
-
-UngweUI.debug = false  -- Debug standardmäßig aus
-
-function UngweUI:Debug(...)
-  if self.debug then
-    local msg = "[UngweUI Debug] "
-    for i = 1, select("#", ...) do
-      msg = msg .. tostring(select(i, ...)) .. " "
-    end
-    DEFAULT_CHAT_FRAME:AddMessage(msg, 0.6, 0.8, 1.0)
-  end
-end
-
-
+UngweUI = UngweUI or {}
 UngweUI.modules = {}
 UngweUI.registered = {}
 
@@ -35,24 +21,9 @@ function UngweUI:InitializeModules()
   end
 end
 
--- Starte Module nach Login
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_LOGIN")
 f:SetScript("OnEvent", function()
   print("UngweUI geladen.")
   UngweUI:InitializeModules()
 end)
-
-
-SLASH_UNGWEUI = "/ungweui"
-SlashCmdList["UNGWEUI"] = function(msg)
-  local command = string.lower(msg)
-
-  if command == "debug" then
-    UngweUI.debug = not UngweUI.debug
-    print("UngweUI Debugmodus: " .. (UngweUI.debug and "Aktiviert" or "Deaktiviert"))
-  else
-    print("UngweUI Befehle:")
-    print("/ungeui debug - Debugmodus ein/aus")
-  end
-end
